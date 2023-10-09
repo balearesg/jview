fe1cdba842236126c7ba36a405e1ff62e8a7b976
FranciscoSanvicente <franciscoemmanuelsanvicente@gmail.com>
2023-10-09 14:26:47 -0400

    Fix: permissions item and access object in item

--- project/modules/components/jview/code/ts/table/item/actions.tsx
+++ project/modules/components/jview/code/ts/table/item/actions.tsx
@@ -2,22 +2,42 @@ import React, { MouseEvent } from "react";
 import { useJViewContext } from "../../context";
 import { IconButton } from "pragmate-ui/icons";
 import { Switch } from "pragmate-ui/form";
+import { routing } from '@beyond-js/kernel/routing';
 
 export function Actions({ item }): JSX.Element {
-    const { actions } = useJViewContext();
+    const { actions, permissions } = useJViewContext();
+
+    let isEdit = true;
+    let isDelete = true;
+    if (permissions && typeof permissions === "object") {
+        const isPermissionWithState = !!permissions.stateId
+        const permission = isPermissionWithState ? permissions.statesPermissions.get(item.stateId) : permissions;
+        isEdit = !!permission.writeP && (permissions.additional ? item.stateId !== permissions.additional : true);
+        isDelete = !!permission.deleteP
+    };
     if (!actions || typeof actions !== "object") return null;
-    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    const handleOnEdit = () => {
+        const url = `${actions.edit.url}/${item.id}${!isEdit ? "?read=true" : ""}`
+        routing.pushState(url);
+    };
+    const handleClick = (event: MouseEvent<HTMLButtonElement>): void => {
         event.stopPropagation();
         const { action } = event.currentTarget.dataset;
+        if (action === "edit" && !actions[action]?.onClick) {
+            handleOnEdit();
+            return
+        }
         if (!actions[action] || !actions[action]?.onClick || typeof actions[action]?.onClick !== 'function') return;
         actions[action].onClick(item);
     };
+    const icon = isEdit ? "pencil" : "eye"
+
     return (
         <td>
             <div className="d-flex align-center actions-jview">
-                {!!actions.status && <Switch />}
-                {!!actions.delete && <IconButton icon="delete" data-action="delete" onClick={handleClick} />}
-                {!!actions.edit && <IconButton data-action="edit" icon="pencil" onClick={handleClick} />}
+                {!!actions.status && isEdit && <Switch />}
+                {!!actions.delete && isDelete && <IconButton icon="delete" data-action="delete" onClick={handleClick} />}
+                {!!actions.edit && <IconButton data-action="edit" icon={icon} onClick={handleClick} />}
             </div>
         </td>
     );
--- project/modules/components/jview/code/ts/table/item/index.tsx
+++ project/modules/components/jview/code/ts/table/item/index.tsx
@@ -4,16 +4,50 @@ import { Actions } from "./actions";
 
 export function ItemTable({ item }): JSX.Element {
   const { keys } = useJViewContext();
- 
-  const entries = keys.map((key: string) => (
-    <td className="td-items" key={key}>
-      <span>{item[key] ?? "Sin valor"}</span>
-    </td>
-  ));
+
+  const entries = keys.map((key) => {
+    if (typeof key === "object" && !!key.keys && Array.isArray(key.keys)) {
+      let entry = item;
+      key.keys.forEach((access) => {
+        entry = entry[access];
+      });
+      if (key.type && key.type === "date") {
+        const date = new Date(entry).toLocaleString('es-ES', { hour12: true });
+        return (
+          <td className="td-items" key={entry}>
+            <span>{date ?? "Sin valor"}</span>
+          </td>
+        );
+      }
+      if (key.type && key.type === "slice") {
+      
+        const isString = entry && typeof entry === 'string';
+        const length = 25;
+        entry = isString
+          ? entry.length > length ? `${entry.slice(0, length)}...` : entry
+          : 'No posee información asociada';
+        return (
+          <td className="td-items" key={entry}>
+            <span>{entry ?? "Sin valor"}</span>
+          </td>
+        );
+      }
+      return (
+        <td className="td-items" key={entry}>
+          <span>{entry ?? "Sin valor"}</span>
+        </td>
+      );
+    }
+    return (
+      <td className="td-items" key={key}>
+        <span>{item[key] ?? "Sin valor"}</span>
+      </td>
+    );
+  });
   return (
     <tr>
       {entries}
-      <Actions item={item}/>
+      <Actions item={item} />
     </tr>
   );
 }
