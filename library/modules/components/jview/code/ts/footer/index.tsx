cc52906160738e091bc8a5610cd8ea0af22a1107
sanvicente.francisco <fsanvicente@balearesgroup.com>
2024-01-17 16:05:11 -0400

    disbled pagination in loading

--- library/modules/components/jview/code/ts/footer/index.tsx
+++ library/modules/components/jview/code/ts/footer/index.tsx
@@ -4,7 +4,7 @@ import { SelectEntries } from "./select-entries";
 import { Pager } from "./pager";
 
 export function Footer() {
-  const { total, showSelect, texts, current, rows, pages, entries } =
+  const { total, showSelect, texts, current, rows, pages, entries, loading, state } =
     useJViewContext();
   const from = (current - 1) * rows + 1;
   const to = from + rows - 1;
@@ -12,10 +12,12 @@ export function Footer() {
     } ${texts.of} ${total} ${texts.items}`;
   const rowsJView = rows ?? 5;
   const isFooter = React.useMemo(() => (total >= 5 && showSelect), [total, rowsJView]);
- 
-  if (!pages) return null
+  const isLoading = loading || state.controller.fetching
+
+  if (!pages) return null;
+  const cls = `content-pager ${isLoading ? "content-pager__loading" : ""}`
   return (
-    <div className="content-pager ">
+    <div className={cls}>
       {!!isFooter && (
         <div className="footer-jivew">
           {showSelect && <SelectEntries />}
