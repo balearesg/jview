9bb56959edf03179393c20b5a992a85b2de224aa
sanvicente.francisco <fsanvicente@balearesgroup.com>
2024-01-25 16:59:14 -0400

    fIX: fixed select

--- library/modules/components/select/code/ts/options/index.tsx
+++ library/modules/components/select/code/ts/options/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useSelectContext } from "../contex";
 import { Empty } from "./empty";
@@ -8,16 +8,15 @@ import { Item } from "./item";
 export function Options(): JSX.Element {
     const { options, manager, ref } = useSelectContext();
 
-    if (!manager.showOptions) return null;
-    const styles: React.CSSProperties = { maxWidth: ref.current.offsetWidth }
+    const styles: React.CSSProperties = { maxWidth: ref?.current?.offsetWidth, }
     if (!options || !Array.isArray(options) || !options.length)
         return <Empty styles={styles} />;
     const output = options.map((item) => {
         return <Item key={uuidv4()} item={item} />
     });
-
+    const cls = `${manager.showOptions ? "options show" : "options hide"}`
     return (
-        <div className="options" style={styles}>
+        <div className={cls} style={styles}>
             {output}
             {manager.confirmDelete && (
                 <ConfirmModal
