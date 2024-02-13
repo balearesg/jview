c079036b5ca7d20d6e7379367843a33d5fcaca3a
veD-tnayrB <Brayanmc.contacto@gmail.com>
2024-02-13 13:48:01 -0400

    fix: Solve the empty with header case

--- library/modules/components/jview/code/ts/empty-view.tsx
+++ library/modules/components/jview/code/ts/empty-view.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Empty } from "pragmate-ui/empty";
-import { useJViewContext } from "./context";
+import { Empty } from 'pragmate-ui/empty';
+import { useJViewContext } from './context';
 
 export function EmptyView() {
-  const {
-    texts,
-    textEmpty,
-    loading,
-    fetching
-  } = useJViewContext();
+	const { texts, textEmpty, loading, fetching } = useJViewContext();
 
-  if (fetching || loading) return null;
+	if (fetching || loading) return null;
 
-  return (
-    <Empty
-      className="empty-jview"
-      icon="circle-exclamation"
-    ><h3>{textEmpty ?? texts.empty}</h3> </Empty>
-  );
+	return (
+		<Empty className="empty-jview" icon="circle-exclamation">
+			<h3>{textEmpty ?? texts.empty}</h3>{' '}
+		</Empty>
+	);
 }
