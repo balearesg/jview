56931215a4bf29375ed8b7f3a70b154a4adc2f10
veD-tnayrB <Brayanmc.contacto@gmail.com>
2025-01-31 14:56:30 -0400

    fix: set the pager next

--- library/modules/components/jview/code/ts/footer/index.tsx
+++ library/modules/components/jview/code/ts/footer/index.tsx
@@ -1,32 +1,49 @@
-import React from "react";
-import { useJViewContext } from "../context";
-import { SelectEntries } from "./select-entries";
-import { Pager } from "./pager";
+import React from 'react';
+import { useJViewContext } from '../context';
+import { Pager } from './pager';
+import { SelectEntries } from './select-entries';
 
 export function Footer() {
-  const { total, showSelect, texts, current, rows, pages, entries, loading, state } =
-    useJViewContext();
-  const rowsJView = rows ?? 5;
-  const from = React.useMemo(() => (current - 1) * rowsJView + 1, [rowsJView, current]);
-  const to = React.useMemo(() => from + rowsJView - 1, [rowsJView, from]);
-  const showing: string = `${texts.showing} ${from} ${texts.to} ${to > total ? total : to
-    } ${texts.of} ${total} ${texts.items}`;
-  const isFooter = React.useMemo(() => (total >= 5 && showSelect), [total, rowsJView]);
+	const {
+		total,
+		showSelect,
+		texts,
+		current,
+		rows,
+		pages,
+		entries,
+		loading,
+		state,
+		pagerNext,
+	} = useJViewContext();
+	const rowsJView = rows ?? 5;
+	const from = React.useMemo(
+		() => (current - 1) * rowsJView + 1,
+		[rowsJView, current]
+	);
+	const to = React.useMemo(() => from + rowsJView - 1, [rowsJView, from]);
+	const showing: string = `${texts.showing} ${from} ${texts.to} ${
+		to > total ? total : to
+	} ${texts.of} ${total} ${texts.items}`;
+	const isFooter = React.useMemo(
+		() => total >= 5 && showSelect,
+		[total, rowsJView]
+	);
 
-  const isLoading = loading || state.controller.fetching
+	const isLoading = loading || state.controller.fetching;
 
-  if (!pages) return null;
-  const cls = `content-pager ${isLoading ? "content-pager__loading" : ""}`;
+	if (!pages) return null;
+	const cls = `content-pager ${isLoading ? 'content-pager__loading' : ''}`;
 
-  return (
-    <div className={cls}>
-      {!!isFooter && (
-        <div className="footer-jivew">
-          {showSelect && <SelectEntries />}
-          {!!entries.length && showing}
-        </div>
-      )}
-      <Pager />
-    </div>
-  );
+	return (
+		<div className={cls}>
+			{!!isFooter && (
+				<div className="footer-jivew">
+					{showSelect && <SelectEntries />}
+					{!!entries.length && showing}
+				</div>
+			)}
+			{pagerNext && <Pager />}
+		</div>
+	);
 }
--- library/modules/components/jview/code/ts/manager/index.ts
+++ library/modules/components/jview/code/ts/manager/index.ts
@@ -1,163 +1,182 @@
-import { ReactiveModel } from "@beyond-js/reactive/model";
-import { JCall } from "@bgroup/jview/api";
-import { TPropsController } from "../interfaces";
+import { ReactiveModel } from '@beyond-js/reactive/model';
+import { JCall } from '@bgroup/jview/api';
+import { TPropsController } from '../interfaces';
 export /*bundle*/
-    class Manager extends ReactiveModel<Manager> {
-    #total;
-    get total() {
-        return this.#total;
-    };
-    set total(value: number) {
-        this.#total = value
-    }
-    #rows;
-    get rows() {
-        return this.#rows;
-    }
-    #action;
-    get action() {
-        return this.#action;
-    }
-    #current;
-    get current() {
-        return this.#current;
-    }
-    set current(value) {
-        if (value === this.#current) return;
-        this.#current = value;
-        this.triggerEvent();
-    }
-    #entries = [];
-    #allEntries = [];
-    get entries() {
-        return this.#entries.slice(this.#current - 1, this.#current * this.#rows);
-    }
+class Manager extends ReactiveModel<Manager> {
+	#total;
+	get total() {
+		return this.#total;
+	}
+	set total(value: number) {
+		this.#total = value;
+	}
+	#rows;
+	get rows() {
+		return this.#rows;
+	}
+	#action;
+	get action() {
+		return this.#action;
+	}
+	#current;
+	get current() {
+		return this.#current;
+	}
+	set current(value) {
+		if (value === this.#current) return;
+		this.#current = value;
+		this.triggerEvent();
+	}
+	#entries = [];
+	#allEntries = [];
+	get entries() {
+		return this.#entries.slice(
+			this.#current - 1,
+			this.#current * this.#rows
+		);
+	}
 
-    ready = !this.entries && !!this.total && !!this.rows;
+	ready = !this.entries && !!this.total && !!this.rows;
 
-    #pages: number;
-    get pages() {
-        return this.#pages;
-    }
+	#pages: number;
+	get pages() {
+		return this.#pages;
+	}
 
-    #pager: number;
-    get pager() {
-        return this.#pager;
-    }
+	#pager: number;
+	get pager() {
+		return this.#pager;
+	}
 
-    #onPrev;
-    #onNext;
-    #caller: any = new JCall();
-    #props
+	#onPrev;
+	#onNext;
+	#caller: any = new JCall();
+	#props;
 
-    #reverse = {};
+	#reverse = {};
 
-    icon = (key: string) => {
-        return this.#reverse[key] ? "arrowDropUp" : "arrowDropDown";
-    };
+	icon = (key: string) => {
+		return this.#reverse[key] ? 'arrowDropUp' : 'arrowDropDown';
+	};
 
-    constructor(props: TPropsController) {
-        super();
-        const { total, rows, action, current, entries, onNext, onPrev, dataHead } = props;
-        this.#total = total;
-        this.#rows = rows;
-        this.#action = action;
-        this.#current = current ?? 1;
-        this.#entries = entries ?? [];
-        this.#allEntries = entries ?? [];
-        if (total <= rows) this.#pages = 1;
-        else this.#pages = Math.ceil(total / rows);
-        this.#onNext = onNext;
-        this.#onPrev = onPrev;
-        if (dataHead && Array.isArray(dataHead)) {
-            dataHead.forEach(item => this.#reverse[item.id] = false);
-        };
-        this.#props = props;
-    }
-    #ajaxCall: (next: any) => Promise<any> = async (next: any): Promise<any> => {
-        const response: any = await this.#caller.get(this.#action, {
-            next: next,
-            limit: this.#rows,
-        });
-        if (response.status !== "ok" || !response.data) {
-            console.warn("there is no data");
-            return [];
-        }
-        return response.data;
-    };
+	constructor(props: TPropsController) {
+		super();
+		const {
+			total,
+			rows,
+			action,
+			current,
+			entries,
+			onNext,
+			onPrev,
+			dataHead,
+		} = props;
+		this.#total = total;
+		this.#rows = rows;
+		this.#action = action;
+		this.#current = current ?? 1;
+		this.#entries = entries ?? [];
+		this.#allEntries = entries ?? [];
+		if (total <= rows) this.#pages = 1;
+		else this.#pages = Math.ceil(total / rows);
+		this.#onNext = onNext;
+		this.#onPrev = onPrev;
+		if (dataHead && Array.isArray(dataHead)) {
+			dataHead.forEach(item => (this.#reverse[item.id] = false));
+		}
+		this.#props = props;
+	}
+	#ajaxCall: (next: any) => Promise<any> = async (
+		next: any
+	): Promise<any> => {
+		const response: any = await this.#caller.get(this.#action, {
+			next: next,
+			limit: this.#rows,
+		});
+		if (response.status !== 'ok' || !response.data) {
+			console.warn('there is no data');
+			return [];
+		}
+		return response.data;
+	};
 
-    changeOrder = (event) => {
-        const {
-            dataset: { key },
-        } = event.currentTarget;
-        const sort = (a, b) => {
-            if (!a[key] || !b[key]) return;
-            if (this.#reverse[key])
-                return b[key].toLowerCase().localeCompare(a[key].toLowerCase());
-            return a[key].toLowerCase().localeCompare(b[key].toLowerCase());
-        };
-        this.#entries = this.#allEntries.sort(sort);
-        this.#reverse[key] = !this.#reverse[key];
-        this.triggerEvent();
-    };
+	changeOrder = event => {
+		const {
+			dataset: { key },
+		} = event.currentTarget;
+		const sort = (a, b) => {
+			if (!a[key] || !b[key]) return;
+			if (this.#reverse[key])
+				return b[key].toLowerCase().localeCompare(a[key].toLowerCase());
+			return a[key].toLowerCase().localeCompare(b[key].toLowerCase());
+		};
+		this.#entries = this.#allEntries.sort(sort);
+		this.#reverse[key] = !this.#reverse[key];
+		this.triggerEvent();
+	};
 
-    /*
-     * Este metodo espera recibir la data de la llamada o del metodo que ejecuta
-     *
-     * @param page
-     * @param entries
-     * @returns {Promise<void>}
-     */
-    getPage = async (page: number, entries: any[]): Promise<void> => {
-        this.fetching = true;
-        this.triggerEvent();
+	/*
+	 * Este metodo espera recibir la data de la llamada o del metodo que ejecuta
+	 *
+	 * @param page
+	 * @param entries
+	 * @returns {Promise<void>}
+	 */
+	getPage = async (page: number, entries: any[]): Promise<void> => {
+		this.fetching = true;
+		this.triggerEvent();
 
-        if (page < this.#current) {
-            this.#current = page;
-            if (this.#onPrev && typeof this.#onPrev === "function") {
-                await this.#onPrev({ page });
-            }
-            this.fetching = false;
-            this.triggerEvent();
-            return;
-        }
+		if (page < this.#current) {
+			this.#current = page;
+			if (this.#onPrev && typeof this.#onPrev === 'function') {
+				await this.#onPrev({ page });
+			}
+			this.fetching = false;
+			this.triggerEvent();
+			return;
+		}
 
-        const next: number = this.#rows * this.#current;
-        const localItems: number = entries.length;
+		const next: number = this.#rows * this.#current;
+		const localItems: number = entries.length;
 
-        if (localItems > next) {
-            this.#current = page;
-            this.fetching = false;
-            this.triggerEvent();
-            return;
-        }
+		if (localItems > next) {
+			this.#current = page;
+			this.fetching = false;
+			this.triggerEvent();
+			return;
+		}
 
-        const call: Function = this.#onNext ?? this.#ajaxCall;
-        const data: any = await call({ next, page });
-        entries = data;
-        this.#current = page;
+		const call: Function = this.#onNext ?? this.#ajaxCall;
+		const data: any = await call({ next, page });
+		entries = data;
+		this.#current = page;
 
-        this.fetching = false;
-        this.triggerEvent();
-    };
+		this.fetching = false;
+		this.triggerEvent();
+	};
 
-    changeItems = async ({ limit: newLimit, pages: newPages }) => {
-        this.#pages = newPages;
-        this.#rows = newLimit;
-        this.triggerEvent();
-    };
+	changeItems = async ({ limit: newLimit, pages: newPages }) => {
+		this.#pages = newPages;
+		this.#rows = newLimit;
+		this.triggerEvent();
+	};
 
-    handleChangeRows = async ({ limit }): Promise<void> => {
-        this.#rows = limit;
-        let pages: number;
-        if (this.#total <= limit) pages = 1;
-        else pages = Math.ceil(this.#total / limit);
-        this.#current = 1;
-        this.#pages = pages;
-        if (this.#props.load && typeof this.#props.load === "function") {
-            const current = await this.#props.load({ limit, total: this.#total, pages });
-            if (current) this.#current = parseInt(current)
-        }
-        this.triggerEvent();
-    };
+	handleChangeRows = async ({ limit }): Promise<void> => {
+		this.#rows = limit;
+		let pages: number;
+		if (this.#total <= limit) pages = 1;
+		else pages = Math.ceil(this.#total / limit);
+		this.#current = 1;
+		this.#pages = pages;
+
+		if (this.#props.load && typeof this.#props.load === 'function') {
+			const current = await this.#props.load({
+				limit,
+				total: this.#total,
+				pages,
+			});
+			if (current) this.#current = parseInt(current);
+		}
+		this.triggerEvent();
+	};
 }
